ee01f6e387d895671ffa0e9cb05dc844ceddc27e
Chris <chriscrawf95@gmail.com>
2016-07-01 23:22:56 -0400

    Revert "Notifications: rework floating blacklist"
    This reverts commit a04404a0baa485b7a8563747bb7021d1ed2d0306.

--- src/com/android/settings/notification/NotificationBackend.java
+++ src/com/android/settings/notification/NotificationBackend.java
@@ -46,7 +46,7 @@ public class NotificationBackend {
         row.banned = getNotificationsBanned(row.pkg, row.uid);
         row.priority = getHighPriority(row.pkg, row.uid);
         row.peekable = getPeekable(row.pkg, row.uid);
-		row.floating = getFloating(row.pkg, row.uid);
+		row.floating = getFloating(row.pkg);
         row.sensitive = getSensitive(row.pkg, row.uid);
         return row;
     }
@@ -110,18 +110,18 @@ public class NotificationBackend {
         }
     }
 	
-	public boolean getFloating(String pkg, int uid) {
+	public boolean getFloating(String pkg) {
         try {
-            return sINM.getPackageFloating(pkg, uid);
+            return sINM.isPackageAllowedForFloatingMode(pkg);
         } catch (Exception e) {
             Log.w(TAG, "Error calling NoMan", e);
             return false;
         }
     }
 
-    public boolean setFloating(String pkg, int uid, boolean floating) {
+    public boolean setFloating(String pkg, boolean status) {
         try {
-            sINM.setPackageFloating(pkg, uid, floating);
+            sINM.setFloatingModeBlacklistStatus(pkg, status);
             return true;
         } catch (Exception e) {
            Log.w(TAG, "Error calling NoMan", e);
