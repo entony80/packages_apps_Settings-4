f2ff6dccf7475bc7925d31dcf48b93a6d7980978
Chris <chriscrawf95@gmail.com>
2016-07-13 23:42:11 -0400

    Settings: 4G icon (Instead of LTE)
    - Modified for CypherOS
    - Clean up class to support custom views

--- src/com/android/settings/cypher/fragments/StatusBarIcons.java
+++ src/com/android/settings/cypher/fragments/StatusBarIcons.java
@@ -18,6 +18,7 @@ package com.android.settings.cypher.fragments;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.net.Uri;
@@ -29,8 +30,9 @@ import android.preference.PreferenceScreen;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
 
-import com.android.settings.SettingsPreferenceFragment;
+import android.provider.Settings;
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import java.util.ArrayList;
@@ -38,17 +40,44 @@ import java.util.List;
 import java.util.Collections;
 
 import com.android.internal.logging.MetricsLogger;
+import com.android.internal.util.aicp.AicpUtils;
 
 public class StatusBarIcons extends SettingsPreferenceFragment {
 	
 	private static final String TAG = StatusBarIcons.class.getSimpleName();
 	
+	private static final String SHOW_FOURG = "show_fourg";
+	
+	private SwitchPreference mShowFourG;
+	
+	private boolean mCheckPreferences;
+	
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+		createCustomView();
+	}
+		
+	private PreferenceScreen createCustomView() {
 
+		mCheckPreferences = false;
         addPreferencesFromResource(R.xml.status_bar_icons);
-        final PreferenceScreen prefScreen = getPreferenceScreen();
+		
+		PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+        Context context = getActivity();
+		
+		// Show 4G
+        mShowFourG = (SwitchPreference) findPreference(SHOW_FOURG);
+        if (AicpUtils.isWifiOnly(getActivity())) {
+            prefSet.removePreference(mShowFourG);
+        } else {
+           mShowFourG.setChecked((Settings.System.getInt(resolver,
+           Settings.System.SHOW_FOURG, 0) == 1));
+        }
+		
+		mCheckPreferences = true;
+        return prefSet;
     }
 	
 	@Override
@@ -56,4 +85,15 @@ public class StatusBarIcons extends SettingsPreferenceFragment {
         // todo add a constant in MetricsLogger.java
         return MetricsLogger.APPLICATION;
 	}
+	
+	@Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mShowFourG) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SHOW_FOURG, checked ? 1:0);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
 }
