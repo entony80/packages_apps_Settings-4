559ab28d8b4ad45c159de26292f4f6aa18010da8
Chris <chriscrawf95@gmail.com>
2016-07-26 00:10:50 -0400

    SmartBar: Possibly fix NPE when adding custom actions
    This is my best guess at what's causing the crash. It's calling for a
    native resource without the proper import

--- src/com/android/settings/aicp/ActionPickerDialogActivity.java
+++ src/com/android/settings/aicp/ActionPickerDialogActivity.java
@@ -29,6 +29,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 
